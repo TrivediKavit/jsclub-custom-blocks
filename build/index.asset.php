944d528026d24d44c47d30190bec0199aad7f51c
Kavit Trivedi <kavit.trivedi@automattic.com>
2024-02-28 12:25:27 +0530

    Init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ad603c81d28f4c34ce74');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '258a4efa7d70dc5cc0cb');
